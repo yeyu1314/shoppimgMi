2a7ba41c94e1d5094ddc36dce5324c17ad361fd4
“2454425375@qq.com <2454425375@qq.com>
2020-06-19 10:46:12 +0800

    '路由'

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    index.vue
+  </div>
+</template>
+<script>
+  export default{
+    name:'index'
+  }
+</script>
---
+++ src/router.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './pages/home'
+import Index from './pages/index'
+Vue.use(Router);
+
+export default new Router({
+  routes:[
+    {
+      path:'/',
+      name:'home',
+      component:Home,
+      redirect:'/index',
+      children:[
+        {
+          path: '/index',
+          name: 'index',
+          component: Index,
+        }, {
+          path: '/product/:id',
+          name: 'product',
+          component: () => import('./pages/productList/product.vue')
+        }, {
+          path: '/detail/:id',
+          name: 'detail',
+          component: () => import('./pages/productList/detail.vue')
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./pages/login.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('./pages/productList/cart.vue')
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: () => import('./pages/orderList/order.vue'),
+      children:[
+        {
+          path: 'list',
+          name: 'order-list',
+          component: () => import('./pages/orderList/orderList.vue')
+        },
+        {
+          path: 'confirm',
+          name: 'order-confirm',
+          component: () => import('./pages/orderList/orderConfirm.vue')
+        },
+        {
+          path: 'pay',
+          name: 'order-pay',
+          component: () => import('./pages/orderList/orderPay.vue')
+        },
+        {
+          path: 'alipay',
+          name: 'alipay',
+          component: () => import('./pages/productList/alipay.vue')
+        }
+      ]
+    }
+  ]
+});
