2da24f4c1b24576f4ce48054b19b37fc7cd3e026
yeyu1314 <2454425375@qq.com>
2020-06-26 21:50:21 +0800

    nav-footer

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,8 +1,54 @@
 <template>
-  <div>nav-footer</div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p href="">小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="">微微夜雨博客</a><span>|</span>
+      <a href="">微微夜雨博客</a><span>|</span>
+      <a href="">微微夜雨博客</a><span>|</span>
+      <a href="">微微夜雨博客</a>
+    </div>
+    <div class="copyright">Copyright ©2019 <span class="domain">mi.futurefe.com</span> All Rights Reserved.</div>
+  </div>
 </template>
 <script>
   export default{
     name:'nav-footer'
   }
 </script>
+<style lang="scss">
+.footer {
+  height: 234px;
+  border-top: 4px solid #ff6600;
+  background-color: #333;
+  color: #999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo {
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img{
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px；
+    }
+  }
+  .footer-link{
+      a{
+        color:#999999;
+        display:inline-block;
+      }
+      span{
+        margin:0 10px;
+      }
+      margin-bottom:13px;
+    }
+    .copyright{
+      .domain{
+        color:#FF6600;
+      }
+    }
+}
+</style>
