b1ac64f48ed191c6ee2b2888426dd69f07f733c3
“2454425375@qq.com <2454425375@qq.com>
2020-07-03 17:33:21 +0800

    '吸顶'

---
+++ src/components/ProductParam.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="nav-bar" :class="{'is_fixed':isFixed}">
+        <div class="container">
+            <div class="pro-title">小米</div>
+            <div class="pro-param">
+                <a href="javascript:;">概述</a><span>|</span>
+                <a href="javascript:;">参数</a><span>|</span>
+                <a href="javascript:;">用户评价</a>
+                <slot name="buy"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'nav-bar',
+    props:{
+      title:String
+    },
+    data(){
+      return {
+        isFixed:false
+      }
+    },
+    mounted(){
+        // 添加吸顶效果
+      window.addEventListener('scroll',this.initHeight)
+    },
+    methods:{
+      initHeight(){
+          // 获取滚动条高度(Y轴 / chrome / ie)
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+        this.isFixed = scrollTop > 152? true:false;
+      }
+    },
+    destroyed(){ // 清除吸顶方法
+      window.removeEventListener('scroll',this.initHeight,false)
+    }
+}
+</script>
+<style lang="scss">
+@import './../assets/scss/config.scss';
+@import './../assets/scss/mixin.scss';
+  .nav-bar{
+    height:70px;
+    line-height:70px;
+    border-top:1px solid $colorH;
+    background-color:$colorG;
+    z-index:10;
+    &.is_fixed{
+      position:fixed;
+      top:0;
+      width:100%;
+      box-shadow: 0 5px 5px $colorE;
+    }
+    .container{
+      @include flex();
+      .pro-title{
+        font-size:$fontH;
+        color:$colorB;
+        font-weight:bold;
+      }
+      .pro-param{
+        font-size:$fontJ;
+        span{
+          margin:0 10px;
+        }
+        a{
+          color:$colorC;
+        }
+      }
+    }
+  }
+</style>
--- src/pages/productList/product.vue
+++ src/pages/productList/product.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
-    product
+  <div class="product">
+    <product-param></product-param>
+    <div class="content">
+      <p>123</p>
+    </div>
   </div>
 </template>
 <script>
+import ProductParam from './../../components/ProductParam'
   export default{
-    name:'product'
+    name:'product',
+    data(){
+      return {
+
+      }
+    },
+    components: {
+      ProductParam
+    }
   }
 </script>
